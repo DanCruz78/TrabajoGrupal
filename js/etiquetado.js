54ba3fc9abf8f0684c1658b895c044c9b6b7c2b3
Lucia Lopez <luli.lopezordines@gmail.com>
2023-05-11 21:41:03 -0300

    comentarios en js

--- js/etiquetado.js
+++ js/etiquetado.js
@@ -1,8 +1,10 @@
 //FORMULARIO --------------------------------
 
+//lo que va a permitir que se conecte al formulario
 const formulario = document.getElementById('formulario_');
 const inputs = document.querySelectorAll('#formulario_ input');
 
+//objeto con propiedades que va a permitir lo que el usuario pueda colocar
 const expresiones = {
     //que comience y termine con algunos de esos caracteres; y tenga esa cantidad de caracteres
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
@@ -33,6 +35,8 @@ const validarFormulario = (e) => {
     }
 }
 
+
+//lo que sucede cuando escribo en los inputs
 const validarCampo = (expresion, input, campo) => {
     if(expresion.test(input.value)){
         document.getElementById(`grupo_${campo}`).classList.add('formulario_grupo-correcto');
@@ -51,12 +55,14 @@ const validarCampo = (expresion, input, campo) => {
     }
 }
 
+//por cada inputs ejecute un código
 inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
+    input.addEventListener('keyup', validarFormulario); //cuando presiono una tecla ya esta ejecutando la función
+    input.addEventListener('blur', validarFormulario); //cuando escribo y presiono cualquier parte de la pantalla ya esta validando
 
 });
 
+//lo que sucede cuando presiono el botón de enviar
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
@@ -64,10 +70,12 @@ formulario.addEventListener('submit', (e) => {
         formulario.reset();
 
         document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
-		setTimeout(() => {
+		//setimeout para que el mensaje se elimine al cabo de unos segundos
+        setTimeout(() => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
 		}, 5000);
 
+        //para que se elimines los iconos
 		document.querySelectorAll('.formulario_grupo-correcto').forEach((icono) => {
 			icono.classList.remove('formulario_grupo-correcto');
 		});
